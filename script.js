8eee4d75d381d48e0a205c9d15b24fa3530b0991
Joao Pedro <joaopedroluz600@gmail.com>
2026-03-30 17:38:04 -0300

    Restaurando tudo

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Exemplo: Mostrar uma mensagem quando o usuário clica em uma seção
-document.querySelectorAll('.section').forEach(item => {
-    item.addEventListener('click', () => {
-        console.log("Você clicou em uma seção do mapa mental!");
-    });
+document.addEventListener('DOMContentLoaded', () => {
+    lucide.createIcons();
 });
